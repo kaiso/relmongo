f4e513e29d643f2c24509160447ea90d34647270
Kais OMRI <kais.omri@parts-advisor.com>
2018-05-17 18:33:38 +0200

    Fix @Id seach problem on superClass

--- src/main/java/io/github/kaiso/relmongo/lazy/LazyLoadingCallback.java
+++ src/main/java/io/github/kaiso/relmongo/lazy/LazyLoadingCallback.java
@@ -7,11 +7,13 @@ import org.springframework.cglib.proxy.LazyLoader;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.MongoOperations;
 import org.springframework.util.ReflectionUtils;
+import org.springframework.util.ReflectionUtils.FieldCallback;
 
 import java.lang.reflect.Field;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class LazyLoadingCallback implements LazyLoader {
@@ -30,8 +32,7 @@ public class LazyLoadingCallback implements LazyLoader {
     public Object loadObject() throws Exception {
 
         if (Collection.class.isAssignableFrom(original.getClass()) && !(original instanceof LazyLoadingProxy)) {
-            List<ObjectId> idlist = (List<ObjectId>) ((Collection) original).stream().map(this::getIdFromObject)
-                    .collect(Collectors.toList());
+            List<ObjectId> idlist = (List<ObjectId>) ((Collection) original).stream().map(this::getIdFromObject).collect(Collectors.toList());
             if (!idlist.isEmpty()) {
                 return DatabaseLoader.findByIds(mongoOperations, ((Collection) original).iterator().next().getClass(),
                         idlist.toArray(new ObjectId[idlist.size()]));
@@ -41,26 +42,46 @@ public class LazyLoadingCallback implements LazyLoader {
     }
 
     private ObjectId getIdFromObject(Object obj) {
-        for (Field field : obj.getClass().getDeclaredFields()) {
+
+        ObjectIdReaderCallback objectIdReaderCallback = new ObjectIdReaderCallback(obj);
+        ReflectionUtils.doWithFields(obj.getClass(), objectIdReaderCallback);
+        return objectIdReaderCallback.getObjectId()
+                .orElseThrow(() -> new IllegalStateException("can not find the @Id field in the referenced entity " + obj.getClass()));
+
+    }
+
+    private class ObjectIdReaderCallback implements FieldCallback {
+
+        private ObjectId objectId;
+        private Object source;
+
+        public ObjectIdReaderCallback(Object source) {
+            this.source = source;
+        }
+
+        @Override
+        public void doWith(Field field) throws IllegalArgumentException, IllegalAccessException {
             if (field.isAnnotationPresent(Id.class)) {
                 ReflectionUtils.makeAccessible(field);
                 try {
-                    Object value = field.get(obj);
-                    ObjectId id;
+                    Object value = field.get(source);
                     if (value instanceof String) {
-                        id = new ObjectId((String) value);
+                        objectId = new ObjectId((String) value);
                     } else {
-                        id = (ObjectId) value;
+                        objectId = (ObjectId) value;
                     }
-                    return id;
                 } catch (IllegalArgumentException | IllegalAccessException e) {
                     throw new IllegalStateException("unable to access the @Id field", e);
                 } catch (ClassCastException e) {
                     throw new IllegalStateException("the @Id field must be of type ObjectId or String", e);
                 }
             }
+
         }
-        throw new IllegalStateException("can not find the @Id field");
+
+        public Optional<ObjectId> getObjectId() {
+            return Optional.ofNullable(objectId);
+        }
 
     }
 
